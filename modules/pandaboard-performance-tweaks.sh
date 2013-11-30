350962ca7b643871088808eb2aa7a9a89b6a59f4
Conor Mac Aoidh <conormacaoidh@gmail.com>
2013-11-30 13:06:51 +0000

    changes to pandaboard-performance-tweaks module

--- modules/pandaboard-performance-tweaks.sh
+++ modules/pandaboard-performance-tweaks.sh
@@ -5,9 +5,16 @@
 #
 # @author Conor Mac Aoidh <conormacaoidh@gmail.com>
 
+# enable performace cpu govenor
+update-rc.d ondemand disable
+apt-get -y install cpufrequtils
+echo 'ENABLE="true"
+GOVERNOR="performance"
+MAX_SPEED="0"
+MIN_SPEED="0"' > /etc/default/cpufrequtils
+cpufreq-set -r -g performance
 
-
-sudo sh -c "echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+# disable swap, use tmpfs
 sed -i 's/\/SWAP/#\/SWAP/g' /etc/fstab
 echo "tmpfs /tmp tmpfs nodev,nosuid 0 0
 tmpfs /var/log tmpfs nodev,nosuid 0 0
